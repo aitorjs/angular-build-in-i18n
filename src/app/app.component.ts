4fe80f45384b1f3962626e7de7d2cc9820dc0174
Aitor Ibañez <aitiba@gmail.com>
2017-06-25 01:44:20 +0200

    - Added select examples.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   logo = 'https://raw.githubusercontent.com/colmena/colmena/develop/apps/admin/src/assets/logo.png';
   wolves = 0;
+  gender = 'f';
+  count = 2;
+  heroes: string[] = ['Magneta', 'Celeritas', 'Dynama'];
 
   inc(i: number) {
     this.wolves = Math.min(5, Math.max(0, this.wolves + i));
+  }
+  male() {
+    this.gender = 'm';
+  }
+  female() {
+    this.gender = 'f';
   }
 }
